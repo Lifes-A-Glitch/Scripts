2b7ff571a73bbfffdde4e362fdfd48540a1acf37
Alexander Istrati-Brown <b.alexander.alex886@gmail.com>
2022-01-30 14:50:03 -0600

    Adding options into the bash script

--- WOL-Script/src/WOL.sh
+++ WOL-Script/src/WOL.sh
@@ -6,7 +6,7 @@ Help() {
     # Display Help
     echo "Sends Wake On Lan packet if ping comes back with no connection
    
-        Syntax: scriptTemplate [-p|a|m|V]
+        Syntax: scriptTemplate [-p|a|m|v]
         Options:
             p       Designated for the port, defaulted to 9.
             a       Designated for the ip address
@@ -27,9 +27,21 @@ while getopts "pamhv" option; do
         Help
         exit
         ;;
-    v)  # display version
+    v) # display version
         echo "Software Version: $version"
         # echo "In the version option..."
+        exit
+        ;;
+    a) # set ip address #TODO: and possibly display it?...
+        echo "Setting IP Address..."
+        exit
+        ;;
+    m) # set MAC address #TODO: and possibly display it?...
+        echo "Setting MAC Address..."
+        exit
+        ;;
+    p) # set port #TODO: and possibly display it?...
+        echo "Setting Port Number..."
         exit
         ;;
     \?) # Invalid option
