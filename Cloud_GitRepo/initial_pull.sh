462f51c89d0ba64d8194cfd60640d2109d0ec550
Callitreality <48292684+Callitreality@users.noreply.github.com>
2022-02-06 22:17:55 -0600

    Changes on Ubuntu VM
    added new function 'making_tree_structure' that allows for file structure to be made in case if it's not created. Still need works though.

--- Cloud_GitRepo/initial_pull.sh
+++ Cloud_GitRepo/initial_pull.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 # Initial Pull of all github repos from a .txt file
+making_tree_structure()
+{
+    printf "\n$(pwd)\n"
+    if [[ ! -e GithubRepo ]]; then
+    {
+        mkdir $(pwd)/GithubRepo
+        cd "$(pwd)/GithubRepo"
+        mkdir scripts
+        echo "GithubRepo/scripts is made..."
+        mkdir repos
+        echo "GithubRepo/repos is made..."
+        mkdir logs
+        echo "GithubRepo/logs is made..."
+    }
+    fi
+}
+
 initialpull() {
     # append gh-repo-list.txt file for more repos...
     input="/home/pi/USB/GithubRepo/scripts/gh-repo-list.txt"
@@ -9,7 +26,9 @@ initialpull() {
     done <"$input"
 }
 
-initialpull >> /home/pi/USB/GithubRepo/logs/"InitialPullOfRepos_$(date +"%b-%d-%Y")".txt
+making_tree_structure
+
+#initialpull #>> /home/pi/USB/GithubRepo/logs/"InitialPullOfRepos_$(date +"%b-%d-%Y")".txt
 
 # # TODO: for cloud-based services... add premade file structure making function.  
 # remove script since its only needed for a massive pull.
